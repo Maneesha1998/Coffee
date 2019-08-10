af320b985239f885d0c2a13da827e672035166db
Maneesha1998 <53978758+Maneesha1998@users.noreply.github.com>
2019-08-10 23:17:11 +0530

    rename READ.ME to coffe.py

---
+++ coffee.py
@@ -0,0 +1,22 @@
+The coffee shop that you use often has a service that is limited today.
+
+- When purchasing coffee, the price for the next purchase will be P% off.
+- Truncation after decimal point in each price reduction
+
+You have noticed that the price cuts are cumulative.
+If you drink coffee many times, you will be able to drink coffee for free.
+
+You want to drink coffee for free, so you will calculate how many INR you can pay for later.
+
+Let's actually write a program and calculate it.
+
+Using Python
+
+x,p=
+sum=0
+p=100-p
+while(x>0):
+  sum=sum+x
+  x=int((p*x)/100)
+  
+print(sum)
